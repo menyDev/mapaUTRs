b5ed82cdcb4b80c17e080c019cdfdf4e996ed1d1
JMAS Parral Mecatron <meny_mar@hotmail.com>
2022-08-08 14:52:24 -0600

    Se añade archivos para heroku

--- build/app.js
+++ build/app.js
@@ -1,3 +1,27 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,21 +31,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 //importación de paquetes
-import * as CONSTANTES from './constantes.js';
-import fetch from 'node-fetch';
+const CONSTANTES = __importStar(require("../constantes"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 //decalaración de variables y constantes
 //solicita valores de UTRs
 function getUTRs() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             // 👇️ const response: Response
-            const response = yield fetch(CONSTANTES.getUTRsURL, {
+            const response = yield (0, node_fetch_1.default)(CONSTANTES.getUTRsURL, {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
---
+++ build/constantes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.lonParral = exports.latParral = exports.getUTRsURL = exports.urlBase = void 0;
+//para API
+exports.urlBase = '';
+exports.getUTRsURL = exports.urlBase + "/getUTRs";
+//lat y long de parral
+exports.latParral = 26.93232;
+exports.lonParral = -105.66668;
